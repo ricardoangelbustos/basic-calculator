04502c5a3e68e2bce07c4cad7807b9014a4c3faa
Ricardo Bustos <ricardobustos14@outlook.com>
2020-06-29 23:00:39 -0300

    Agregados listeners teclado

--- script.js
+++ script.js
@@ -21,3 +21,88 @@ function calc() {
 function del() { 
     setResult(0);
 }
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 97) {
+        document.getElementById("1").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 98) {
+        document.getElementById("2").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 99) {
+        document.getElementById("3").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 100) {
+        document.getElementById("4").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 101) {
+        document.getElementById("5").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 102) {
+        document.getElementById("6").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 103) {
+        document.getElementById("7").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 104) {
+        document.getElementById("8").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 105) {
+        document.getElementById("9").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 96) {
+        document.getElementById("0").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 107) {
+        document.getElementById("+").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 109) {
+        document.getElementById("-").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 106) {
+        document.getElementById("*").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 111) {
+        document.getElementById("/").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 8) {
+        document.getElementById("del").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 110) {
+        document.getElementById("point").click();
+    }
+}, false)
+document.addEventListener('keyup', event => {
+    if (event.keyCode === 13) {
+        document.getElementById("equal").click();
+    }
+}, false)
